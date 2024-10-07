1cbd3347ddc286f23fd6d6db13072ab0bbec55ab
Biel Puga Jodas <bielp@81223290175.local>
2024-10-07 16:14:25 +0200

    dia 7 d'octubre, ex 4

--- script1BB.sh
+++ script1BB.sh
@@ -2,9 +2,13 @@
 
 cut -d',' -f 1-11, 13-15 supervivents.csv > sortida.csv
 
-while [ $((1+1)) -eq 2 ] 
-do
+awk {if ($video_error_or_removed !=  True) print} sortida.csv | wc -l
 
+awk {NR == Ranking_Views}
 
-done 
+awk {if ($Views <= 1000000) $Ranking_Views = "Bo"; elif ($Views <= 10000000) $Ranking_Views = "Exel·lent";
+ else ($Ranking_Views = "Estrella"} sortida.csv
+
+for 
+($8 / $9)*100
 
