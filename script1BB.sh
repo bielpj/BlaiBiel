e896f000962ddc0ac706b8bb3e0c5ef178df265d
Biel Puga Jodas <bielp@81223290175.local>
2024-10-14 18:17:23 +0200

    Càrrega amb punt 4 ja fet, falten arreglos

--- script1BB.sh
+++ script1BB.sh
@@ -1,14 +1,42 @@
 #!/bin/bash
 
-cut -d',' -f 1-11, 13-15 supervivents.csv > sortida.csv
+if (($# eq 0)); then
 
-awk {if ($video_error_or_removed !=  True) print} sortida.csv | wc -l
+	cut -d',' -f 1-11, 13-15 supervivents.csv > sortida.csv
 
-awk {NR == Ranking_Views}
+	awk {if ($video_error_or_removed !=  True) print} sortida.csv | wc -l
 
-awk {if ($Views <= 1000000) $Ranking_Views = "Bo"; elif ($Views <= 10000000) $Ranking_Views = "Exel·lent";
- else ($Ranking_Views = "Estrella"} sortida.csv
+	awk {NR == Ranking_Views}
 
-for 
-($8 / $9)*100
+	awk {if ($Views <= 1000000) $Ranking_Views = "Bo"; elif ($Views <= 10000000) $Ranking_Views = "Exel·lent";
+ 		else ($Ranking_Views = "Estrella"} sortida.csv"
+
+	awk -F',' 'NR==1 {print $0",Rlikes,Rdislikes"}' "supervivents.csv" > "sortida.csv"
+
+	numero_lines=$(wc -l < "$supervivents.csv")
+
+	for (( i = 2; i <= numero_lines; i++ )); do
+    		line=$(sed -n "${i}p" "$supervivents.csv")
+    
+     		echo "$line"  
+
+		views=$(echo "$line" | cut -d',' -f8)   
+    		likes=$(echo "$line" | cut -d',' -f9)   
+    		dislikes=$(echo "$line" | cut -d',' -f10) 
+    
+    		if (( views > 0 )); then
+        	Rlikes=$(( (likes * 100) / views ))
+        	Rdislikes=$(( (dislikes * 100) / views ))
+    	fi
+
+    	echo "$line,$Rlikes,$Rdislikes" >> "sortida.csv"
+	done
+
+
+        print $0 "," Rlikes "," Rdislikes;
+    }' >> "sortida.csv"
+done
+
+
+
 
